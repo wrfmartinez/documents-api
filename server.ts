2e33c763453cf2552a103a08838b4e6358cdda5a
wrfmartinez <wrmartinez709@gmail.com>
2024-06-17 20:04:10 -0400

    Add error handling and refactor edit route

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ declare const process: {
 
 dotenv.config();
 const app = express();
-const PORT: number = process.env.PORT | 3000;
+const PORT: number = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -21,30 +21,49 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to ${mongoose.connection.name}`);
 });
+mongoose.connection.on("error", (err) => {
+  console.error(`Error connecting to the database: ${err}`);
+});
 
 app.get("/", async (req, res) => {
-  const documents = await Document.find();
-  res.status(200).json(documents);
+  try {
+    const documents = await Document.find();
+    res.status(200).json(documents);
+  } catch (err: any) {
+    res.status(500).json({error: err.message });
+  }
 });
 
 app.post("/document/new", async (req, res) => {
-  const newDocument = await Document.create(req.body);
-  await newDocument.save();
-  res.status(201).json(newDocument);
+  try {
+    const newDocument = await Document.create(req.body);
+    await newDocument.save();
+    res.status(201).json(newDocument);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
 })
 
 app.put("/document/:documentId", async (req, res) => {
-  const editDocument = await Document.findByIdAndUpdate(req.params.documentId,
-    {
-      $set: req.body
-    }
-  );
-  res.status(201).json(editDocument);
+  try {
+    const editDocument = await Document.findByIdAndUpdate(
+      req.params.documentId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    res.status(200).json(editDocument);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message })
+  }
 })
 
 app.delete("/document/:documentId", async (req, res) => {
-  const deleteDocument = await Document.findByIdAndDelete(req.params.documentId);
+  try {
+    const deleteDocument = await Document.findByIdAndDelete(req.params.documentId);
   res.status(200).json(deleteDocument);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message })
+  }
 })
 
 app.listen(PORT, (): void => {
